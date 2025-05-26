91fdd2e90fe14946eaaeb9d349341d3c5df2446c
Kelvin Obidozie <kelvinobidozie@programmer.net>
2025-05-26 17:00:58 +0100

    compiled hardhat successfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,20 @@ require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.28",
   networks: {
-    goerli: {
+    sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
   },
 };
